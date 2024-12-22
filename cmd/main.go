49efeffcbe15c748a4285f94fd544efc2367e455
Dided08 <daniel_shag@outlook.com>
2024-12-22 16:26:02 +0300

    main.go

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
+    "github.com/user/repo/internal/handlers"
+)
+
+func main() {
+    r := mux.NewRouter()
+    api := r.PathPrefix("/api/v1").Subrouter()
+    api.HandleFunc("/calculate", handlers.CalculateHandler).Methods("POST")
+
+    log.Println("Starting server on :8080...")
+    log.Fatal(http.ListenAndServe(":8080", r))
+}
